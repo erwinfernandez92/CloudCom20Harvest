ee2924d3d604540153396cb2e8807a7a23390f62
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2016-05-02 21:15:34 +1000

    fix: cycle fetching of the last 2 days over and over using search

--- search.py
+++ search.py
@@ -1,4 +1,6 @@
 import time
+import datetime
+from datetime import date, timedelta, datetime
 from threading import Thread
 
 class Searcher(Thread):
@@ -11,12 +13,16 @@ class Searcher(Thread):
     def run(self):
         # first get the lowest tweet, we assume we need to fetch more tweets
         # before this tweet
-        earliestTweet = self.db.earliestId()
-        if earliestTweet != None:
-            print("Fetching tweets before: " + earliestTweet)
+        # earliestTweet = self.db.earliestId()
+        # if earliestTweet != None:
+        #     print("Fetching tweets before: " + earliestTweet)
+        # Above logic is no longer needed as we have featched the last X
+        # days twitter will give us, now we will just recursively run search
+        # on the last 2 days data (and repeat) in order to catch up with
+        # the stream API
 
         # iterate over results, see how many we can fetch
-        max_id = earliestTweet
+        max_id = None # earliestTweet
         count = 0
         while True:
             time.sleep(5)
@@ -38,10 +44,17 @@ class Searcher(Thread):
                 count += len(results)
                 print(result.created_at)
                 print(count)
-                if len(results) == 0:
-                    print("gracefully finishing")
-                    break
+                if len(results) == 0 or olderThanNDays(result.created_at, 2):
+                    print("gracefully restarting to fetching latest")
+                    max_id = None
+                    count = 0
+                    continue
             except Exception as e:
                 print('error running search, trying again in 5 seconds')
                 print(str(e))
                 pass
+
+# utility method
+def olderThanNDays(dateObj, nDays):
+    todayMinusN = datetime.utcnow() - timedelta(days=nDays)
+    return (dateObj < todayMinusN)
